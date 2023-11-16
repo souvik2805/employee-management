48a158a743f7142a9426b8f77de79d96f007eacc
Souvik <souvik2805@gmail.com>
2023-11-16 09:06:53 +0530

    Spring stater setup

---
+++ employee-management-backend/src/test/java/com/souvik2805/employeemanagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souvik2805.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
